aed850fffe28c1d98a3766afb3b8aea5114fdc2e
hs-qwerty <56368718+hs-qwerty@users.noreply.github.com>
2021-09-12 18:53:49 +0300

    Add files via upload
    file upload

---
+++ src/qwerty/ImdbConnectionSchema.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace qwerty;
+
+Interface  ImdbConnectionSchema{
+
+    public function __construct($url);
+
+    public function Connection();
+    public function All();
+    public function AllContinue();
+    public function Also_Known_As();
+    public function Aka_one();
+    public function Aka_two();
+    public function Awards();
+    public function Title();
+    public function Rating();
+    public function Vote();
+    public function Budget();
+    public function Country();
+    public function Language();
+    public function LanguageDetail($var);
+    public function Color();
+    public function ColorDetail($var);
+    public function Aspect_Ratio();
+    public function Aspect_Ratio_Detail($var);
+    public function Official_Sites();
+    public function Official_Sites_Detail($var);
+    public function Runtime();
+    public function Runtime_Details($var);
+    public function Locations();
+    public function Locations_Details($var);
+    public function Certification();
+    public function Certification_Details($var);
+    public function Plot_Summary();
+    public function Plot_Summary_Details($var);
+    public function Plot_Keywords();
+    public function Plot_Keywords_Details($var);
+    public function Taglines();
+    public function Taglines_Details($var);
+    public function Genres();
+    public function Genres_Details($var);
+    public function Sound_Mix();
+    public function Sound_Mix_Details($var);
+    public function Movie_Connections();
+    public function Movie_Connections_Details($var);
+    public function Soundtracks();
+    public function Soundtracks_Details($var);
+    public function Crazy_Credits();
+    public function Crazy_Credits_Details($var);
+    public function Quotes();
+    public function Quotes_Details($var);
+    public function Poster();
+    public function Media($uri);
+    public function Cast();
+
+
+
+}
---
+++ src/test.php
@@ -0,0 +1,73 @@
+<?php
+
+use \qwerty\ImdbCurl as ImdbCurl;
+
+
+require_once "autoload.php";
+
+
+$empty = "https://www.imdb.com/title/tt1056017/releaseinfo";
+
+$a = new ImdbCurl("https://www.imdb.com/title/tt0068646/");
+
+/*
+$all = $a->All();
+$title = $a->Title();
+$rating = $a->Rating();
+$vote = $a->Vote();
+$aka = $a->Also_Known_As();
+$awards = $a->Awards();
+$director = $a->Director();
+$writers = $a->Writers();
+$stars = $a->Stars();
+$budget = $a->Budget();
+$country = $a->Country();
+$lang = $a->Language();
+
+$color = $a->Color();
+
+$aspect = $a->Aspect_Ratio();
+
+$offical = $a->Official_Sites();
+
+$runtime = $a->Runtime_M();
+
+$locations = $a->Locations();
+
+$certificion = $a->Certification();
+
+$plot_summery = $a->Plot_Summary();
+
+$plot_keywords = $a->Plot_Keywords();
+
+$taglines = $a->Taglines();
+
+$genres = $a->Genres();
+
+$sound_mix = $a->Sound_Mix();
+
+$Movie_Connections = $a->Movie_Connections();
+
+$Soundtracks = $a->Soundtracks();
+
+$Crazy_Credist = $a->Crazy_Credits();
+
+$Quotes = $a->Quotes();
+
+
+
+
+
+$Poster = $a->Poster();
+
+
+$Cast = $a->Cast();
+
+
+$awards = $a->Poster();
+
+*/
+$uri = "https://www.imdb.com/title/tt0068646/mediaviewer/rm1703430656/";
+$media = $a->Media($uri);
+
+print_r($media);
